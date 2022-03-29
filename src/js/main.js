510f31fa83cd40ccf4d5ab3f4053931cf5d7be9a
GabrielOJorge <goliver331@protonmail.com>
2022-03-29 18:38:06 -0300

    js: code form validation

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,58 @@
 const form = document.getElementById("main-form");
-const firstName = document.getElementById("f-name");
-const lastName = document.getElementById("l-name");
-const email = document.getElementById("email");
-const pNumber = document.getElementById("p-number");
-const pwd = document.getElementById("pwd");
-const cnfrmPwd = document.getElementById("c-pwd");
 const submitBtn = document.getElementById("submit-btn");
+const formControls = document.querySelectorAll(".form-control");
+
+const formControlsArr = Array.from(formControls);
+
+const setValidFor = input => {
+  const formControl = input.parentElement; 
+  const small = formControl.querySelector("small");
+  
+  small.textContent = null;
+  input.classList.remove("error");
+  input.classList.add("valid");
+};
 
 const setErrorFor = (input, msg) => {
   const formControl = input.parentElement; 
   const small = formControl.querySelector("small");
   
-  small.innerText = msg;
-  input.className = "error";
+  small.textContent = msg;
+  input.classList.remove("valid");
+  input.classList.add("error");
 };
 
-const checkInputs = () => {
-  const firstNameValue = firstName.value.trim();
-  let msg = null;
+const checkPasswords = () => {
+  const pwd = document.getElementById("pwd");
+  const cnfrmPwd = document.getElementById("c-pwd");
 
-  if (!firstNameValue) {
-    msg = "First Name";
+  const pwdValue = pwd.value.trim();
+  const cnfrmPwdValue = cnfrmPwd.value.trim();
+  if (pwdValue !== cnfrmPwdValue) {
+    setErrorFor(pwd, "Passwords do not match!");
+    cnfrmPwd.classList.add("error");
   }
-  
-  firstNameValue ? setSuccessFor(firstName) : setErrorFor(firstName, `${msg} cannot be blank!`);
 };
 
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  checkInputs();
-});
+const checkEmail = () => {
+  const email = document.getElementById("email");
+  const emailValue = email.value.trim();
+  console.log(emailValue);
 
-submitBtn.addEventListener("click", checkInputs);
+  if (emailValue && !emailValue.includes("@")) {
+    setErrorFor(email, "Email must be valid!");
+  }
+};
+
+const checkInputs = e => e.target.value.trim() ? setValidFor(e.target) : setErrorFor(e.target, `This cannot be blank!`);
+
+const validateForm = () => {
+  formControlsArr.map(input => input.value.trim() ? setValidFor(input) : setErrorFor(input, `This cannot be blank!`));
+  checkEmail();
+  checkPasswords();
+};
+
+form.addEventListener("submit", e => e.preventDefault());
+submitBtn.addEventListener("click", validateForm);
+formControlsArr.forEach(control => control.addEventListener("input", checkInputs));
+window.addEventListener("load", () => formControlsArr.forEach(control => control.value = null))
